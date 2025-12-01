690803a1b39680d2ea4e75afdc96c5a3bc468f7e
mguevarahernandez07-lang <mguevarahernandez07@gmail.com>
2025-11-30 22:06:19 -0800

    Add bankAccount class with basic functionality
    Implemented bankAccount class with account management methods.

--- bankAccount.cpp
+++ bankAccount.cpp
@@ -1,1 +1,37 @@
+#include "bankAccount.h"
+
+bankAccount::bankAccount()
+    : accountNumber(0), balance(0.0) {}
+
+bankAccount::bankAccount(int accountNumber, double balance)
+    : accountNumber(accountNumber), balance(balance) {}
+
+void bankAccount::setAccountNumber(int number) {
+    accountNumber = number;
+}
+
+int bankAccount::getAccountNumber() const {
+    return accountNumber;
+}
+
+double bankAccount::getAccountBalance() const {
+    return balance;
+}
+
+void bankAccount::deposit(double amount) {
+    if (amount > 0) {
+        balance += amount;
+    }
+}
+
+void bankAccount::withdraw(double amount) {
+    if (amount > 0) {
+        balance -= amount;
+    }
+}
+
+void bankAccount::print() const {
+    std::cout << "Account Number: " << accountNumber << "\n";
+    std::cout << "Balance: $" << balance << "\n";
+}
 
